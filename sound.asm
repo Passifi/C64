71b68bd167fa50cb21f0c2e1074d2985c0559e65
Pascal Wigger <48407860+Passifi@users.noreply.github.com>
2025-07-18 15:10:30 +0200

    Sound routine added filtering, waveform control, fixed frequnecy setting

--- sound.asm
+++ sound.asm
@@ -2,39 +2,84 @@
 
 .namespace noteValues 
 {
-    .label C_1 = $0218
+    .label C_1 = $022a
+    .label C_4 = $1150
+}
+
+.namespace Waveforms {
+    .label Square = 64 
+    .label Noise = 128 
+    .label Saw = 32
+    .label Triangle = 16
 }
 
 .namespace SID {
     
     .label Voice1FreqLow =SIDBase 
     .label Voice1FreqHigh =SIDBase +1
+    .label Voice1PulsewaveDutyCycleLow =SIDBase +2  
+    .label Voice1PulsewaveDutyCycleHigh =SIDBase+3 
     .label Voice1Waveform = SIDBase+ 4
     .label ADVoice1 = SIDBase + 5
     .label SRVoice1 = SIDBase +6
+    .label filterLow = SIDBase + $15 
+    .label filterHigh = SIDBase + $16
+    .label res_filterCtr = SIDBase + $17 
     .label Volume = SIDBase+24
 }
 
 .macro setNote(note) {
     lda #<note
-    sta SID.Voice1FreqHigh
-    lda #>note
     sta SID.Voice1FreqLow
+    lda #>note
+    sta SID.Voice1FreqHigh
+}
+
+.macro setFilter(value) {
+    lda #<value 
+    sta SID.filterLow
+    lda #>value 
+    sta SID.filterHigh
+}
+
+
+.macro setWaveform(value) {
+   lda #value+1 
+   sta SID.Voice1Waveform 
+}
+
+.macro setDutyCycle(value) {
+    lda #<value 
+    sta SID.Voice1PulsewaveDutyCycleLow
+    lda #>value 
+    sta SID.Voice1PulsewaveDutyCycleHigh
+}
+
+.macro resetSID() {
+    lda #0 
+    .for(var x = 0; x< 25; x++) {
+        sta SIDBase+x
+    }
 }
 
 .macro basicTestASM() {
-    lda #0 
-    .for(var x = 0; x< 24; x++) {
-        sta SIDBase 
-    }
-    lda #15
+    resetSID() 
+    setFilter(1200)
+    setDutyCycle(200)
+    lda #%11110001
+    sta SID.res_filterCtr 
+    lda #%00011111
     sta SID.Volume
-    lda #$09 
+    lda #$1f 
     sta SID.ADVoice1
-    lda #$00
+    lda #$f1
     sta SID.SRVoice1
-    setNote(noteValues.C_1) 
-    lda #17 // lowest bit is gate
-    sta SID.Voice1Waveform
+    setNote(noteValues.C_4)
+    setWaveform(Waveforms.Square)
+filterSweep:
+    jmp filterSweep
 
 }
+
+music:
+    .byte     
